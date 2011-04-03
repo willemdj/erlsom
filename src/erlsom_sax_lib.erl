d02cdd6483d4efc59467b63595579d1193ea47a6
Willem de Jong <w.a.de.jong@gmail.com>
2011-04-03 09:30:43 +0200

    fixed old license in erlsom_sax_lib.erl

--- src/erlsom_sax_lib.erl
+++ src/erlsom_sax_lib.erl
@@ -1,24 +1,25 @@
-%%% ====================================================================
-%%% A couple of functions used by erlsom_sax (for each encoding variant)
-%%% 
-%%% Copyright (C) 2006 Willem de Jong
+%%% Copyright (C) 2006 - 2011 Willem de Jong
 %%%
-%%% This program is free software; you can redistribute it and/or modify
-%%% it under the terms of the GNU General Public License as published by 
-%%% the Free Software Foundation; either version 2 of the License, or 
-%%% (at your option) any later version.
+%%% This file is part of Erlsom.
 %%%
-%%% This program is distributed in the hope that it will be useful, but
-%%% WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-%%% General Public License for more details.
+%%% Erlsom is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU Lesser General Public License as 
+%%% published by the Free Software Foundation, either version 3 of 
+%%% the License, or (at your option) any later version.
 %%%
-%%% You should have received a copy of the GNU General Public License 
-%%% along with this program; if not, write to the Free Software 
-%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-%%% USA
+%%% Erlsom is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU Lesser General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU Lesser General Public 
+%%% License along with Erlsom.  If not, see 
+%%% <http://www.gnu.org/licenses/>.
 %%%
 %%% Author contact: w.a.de.jong@gmail.com
+
+%%% ====================================================================
+%%% A couple of functions used by erlsom_sax (for each encoding variant)
 %%% ====================================================================
 
 %%% Version: 20-01-2008
